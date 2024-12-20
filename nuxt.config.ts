ecd9d1f3755702c48463ea7108c9700f8cad3d4d
Artem Gnedenko <a.gnedenko@idaproject.com>
2024-12-20 21:35:28 +0500

    fix: небольшие правки

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,12 +8,20 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       title: 'Uxpro',
-      // meta: [
-        // process.env.NODE_ENV === 'production' ? {
-        //   'http-equiv': 'Content-Security-Policy',
-        //   content: 'upgrade-insecure-requests',
-        // } : {},
-      // ]
+      meta: [
+        process.env.NODE_ENV === 'production' ? {
+          'http-equiv': 'Content-Security-Policy',
+          content: 'upgrade-insecure-requests',
+        } : {},
+        // <meta name="google-site-verification" content="Gi3XVUeVE1iajZmK4RWzziUDv3YzbC8dys2nPpfBR5k" />
+        { name: 'google-site-verification', content: 'Gi3XVUeVE1iajZmK4RWzziUDv3YzbC8dys2nPpfBR5k' },
+
+        { key: 'og:title', property: 'og:title', content: 'Uxpro' },
+        { key: 'og:description', property: 'og:description', content: 'Uxpro' },
+
+        { key: 'twitter:title', name: 'twitter:title', content: 'Uxpro' },
+        { key: 'twitter:description', name: 'twitter:description', content: 'Uxpro' },
+      ]
     }
   },
 
