68b2745845cb97ce4ac4cb7f02a152ecee293eea
Artem Gnedenko <a.gnedenko@idaproject.com>
2024-12-20 16:06:18 +0500

    feat: поправлен nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,20 @@ import svgLoader from 'vite-svg-loader'
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Uxpro',
+      meta: [
+        {
+          'http-equiv': 'Content-Security-Policy',
+          content: 'upgrade-insecure-requests',
+        }
+      ]
+    }
+  },
+
   css: [
     'normalize.css',
     '@/global/styles/index.css'
