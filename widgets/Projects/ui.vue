87f3868ad1fc4be5813ba4642c7e0216ba02bdef
Artem Gnedenko <a.gnedenko@idaproject.com>
2024-12-25 21:00:01 +0500

    fix: исправления после тестирования

--- widgets/Projects/ui.vue
+++ widgets/Projects/ui.vue
@@ -1,52 +1,60 @@
 <template>
-  <section class="grid grid-cols-2 gap-y-[12.8rem] px-gap mb:px-mbGap mb:flex mb:flex-col mb:gap-[6.4rem]">
+  <section class="flex flex-col gap-[12.8rem] px-gap mb:px-mbGap mb:flex mb:flex-col mb:gap-[6.4rem]">
     <div
-      v-for="(project, index) in props.projects"
-      :key="project.id"
-      :class="['w-full flex', {
-        'self-baseline': index % 2 === 0,
-      }]"
+      v-for="(items, index) in reducedProjects"
+      :key="`wrapper-projects-${index}`"
+      :class="'w-full flex gap-[12.8rem]'"
     >
-      <nuxt-link
-        :class="['group w-max flex flex-col gap-[1.6rem] mb:w-full', {
+      <div
+        v-for="(project, projectIndex) in items"
+        :key="project.id"
+        :class="['grow w-full flex', {
+          'justify-end': index % 2 === 1 && projectIndex % 2 === 1
+        }]"
+      >
+        <nuxt-link
+          :class="['group flex w-fit', {
           'cursor-pointer': project.id === 1,
           'cursor-not-allowed': project.id !== 1,
         }]"
-        :to="project.id === 1 ? `projects/${project.id}` : ''"
-      >
-        <div class="group overflow-hidden">
-          <div
-            :class="['group-hover:scale-105 duration-500 transition-transform overflow-hidden mb:w-[32.8rem] mb:h-[32.8rem]', {
-              'w-[63rem] h-[63rem]': index % 2 === 0,
-              'h-[70.7rem]': index % 2 === 1,
+          :to="project.id === 1 ? `projects/${project.id}` : ''"
+        >
+          <div class="flex flex-col gap-mbGap">
+            <div class="group overflow-hidden">
+              <div
+                :class="['group-hover:scale-105 duration-500 transition-transform overflow-hidden mb:w-[32.8rem] mb:h-[32.8rem]', {
+              'w-[63rem] h-[63rem]': (index % 2 === 0 && projectIndex % 2 === 0) || (index % 2 === 1 && projectIndex % 2 === 1),
+              'h-[70.7rem]': (index % 2 === 1 && projectIndex % 2 === 0) || (index % 2 === 0 && projectIndex % 2 === 1),
             }]"
-          >
-            <NuxtImg
-              :src="project?.preview_image"
-              alt="Project image"
-              class="w-full h-full object-cover"
-            />
+              >
+                <NuxtImg
+                  :src="project?.preview_image"
+                  alt="Project image"
+                  class="w-full h-full object-cover"
+                />
+              </div>
+            </div>
+
+            <div class="group w-max flex items-center gap-[.8rem] t1sb group-hover:text-black group-hover:bg-white transition-colors mb:w-full mb:inline-block">
+              <ArrowRight
+                width="20"
+                height="20"
+                class="min-w-[2rem] mb:inline-block"
+              />
+
+              <p class="text-black bg-white mb:inline-block mb:mx-[.8rem]">
+                {{ project.title }}
+              </p>
+
+              <div class="w-[1rem] h-[1rem] min-w-[1rem] rotate-45 bg-white group-hover:bg-black mb:inline-block mb:mr-[.8rem]" />
+
+              <p class="mb:inline">
+                {{ project.preview_description }}
+              </p>
+            </div>
           </div>
-        </div>
-
-        <div class="group w-max flex items-center gap-[.8rem] t1sb group-hover:text-black group-hover:bg-white transition-colors mb:w-full mb:inline-block">
-          <ArrowRight
-            width="20"
-            height="20"
-            class="min-w-[2rem] mb:inline-block"
-          />
-
-          <p class="text-black bg-white mb:inline-block mb:mx-[.8rem]">
-            {{ project.title }}
-          </p>
-
-          <div class="w-[1rem] h-[1rem] min-w-[1rem] rotate-45 bg-white group-hover:bg-black mb:inline-block mb:mr-[.8rem]" />
-
-          <p class="mb:inline">
-            {{ project.preview_description }}
-          </p>
-        </div>
-      </nuxt-link>
+        </nuxt-link>
+      </div>
     </div>
   </section>
 </template>
@@ -61,4 +69,26 @@ defineOptions({
 const props = defineProps<{
   projects: any[]
 }>()
+
+const reducedProjects = computed(() => {
+  return props.projects.reduce((total, currentValue, currentIndex) => {
+    const lastIndex = total.length ? total.length - 1 : 0;
+
+    const array = total[lastIndex]
+
+    if (!array) {
+      total[lastIndex] = [currentValue]
+    } else {
+      if (array.length === 2) {
+        total[lastIndex + 1] = [currentValue]
+      } else {
+        total[lastIndex].push(currentValue)
+      }
+    }
+
+    return total
+  }, [])
+})
+
+console.log(reducedProjects.value)
 </script>
